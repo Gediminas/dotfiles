cdc32abe76ffe80f8ecad1b342ac8720edadbcdc
Gediminas <gediminas.luzys@gmail.com>
2019-09-23 21:36:30 +0300

    macos: keyboard: comma layer -> numbers

--- .hammerspoon/keyboard/hyper.lua
+++ .hammerspoon/keyboard/hyper.lua
@@ -1,10 +1,11 @@
+local hyper  = {"cmd","alt","ctrl"}
+local shyper = {"cmd","alt","ctrl","shift"}
+
 local status, hyperModeAppMappings = pcall(require, 'keyboard.hyper-apps')
 
 if not status then
   hyperModeAppMappings = require('keyboard.hyper-apps-defaults')
 end
-
-local hyper = {"shift", "cmd", "alt", "ctrl"}
 
 for i, mapping in ipairs(hyperModeAppMappings) do
   local key = mapping[1]
@@ -19,54 +20,5 @@ for i, mapping in ipairs(hyperModeAppMappings) do
     end
   end)
 end
-
-local WifiMsgShown = ""
-local WifiMsg = ""
-
-function showWifiMsg()
-    if WifiMsgShown ~= WifiMsg then
-      -- hs.alert.show(msg)
-      hs.notify.new({title='Hammerspoon', informativeText=WifiMsg}):send()
-      WifiMsgShown = WifiMsg
-    end
-end
-
-function pingWifi(object, message, seqnum, error)
-    if message == "didFinish" then
-        SSID = hs.wifi.currentNetwork()
-        avg = tonumber(string.match(object:summary(), '/(%d+.%d+)/'))
-        if avg == 0.0 then
-            WifiMsg = "🏖️  NO-NETWORK"     .. "  [" .. SSID .. "]"
-        elseif avg < 200.0 then
-            WifiMsg = "🐇 " .. avg .. "ms" .. "  [" .. SSID .. "]"
-        elseif avg < 500.0 then
-            WifiMsg = "🐢 " .. avg .. "ms" .. "  [" .. SSID .. "]"
-        else
-            WifiMsg = "💩 " .. avg .. "ms" .. "  [" .. SSID .. "]"
-        end
-        showWifiMsg()
-    end
-    --hs.notify.new({title='Hammerspoon111', informativeText=object}):send()
-end
-function ssidChangedCallback()
-    SSID = hs.wifi.currentNetwork()
-    if SSID == nil then
-        WifiMsg = "🏖️🏖️   DISCONNECTED"
-        showWifiMsg()
-    else
-        hs.network.ping.ping("8.8.8.8", 1, 0.01, 1.0, "any", pingWifi)
-    end
-end
-
-hs.hotkey.bind(hyper, "0", function()
-   WifiMsgShown = ""  -- Force show message
-   ssidChangedCallback()
-end)
-
-
--- wifiWatcher = hs.wifi.watcher.new(ssidChangedCallback)
--- wifiWatcher:watchingFor({ "SSIDChange", "BSSIDChange", "linkChange", "linkQualityChange", "powerChange" }) --
--- wifiWatcher:start()
--- ssidChangedCallback()
 
 
