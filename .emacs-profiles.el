f710f4d82981bafdcbf4edb6a8eb5ddcdb1241b1
Gediminas <gediminas.luzys@gmail.com>
2019-11-21 17:58:31 +0200

    chemacs: configurations added

---
+++ .emacs-profiles.el
@@ -0,0 +1,6 @@
+(
+ ("default" . ((user-emacs-directory . "~/.emacs.d")))
+ ("old" . ((user-emacs-directory . "~/.emacs.d.old")))
+ ("doom" . ((user-emacs-directory . "~/.emacs.d.doom")))
+ ("bare" . ((user-emacs-directory . "~/.emacs.d.bare")))
+ )
