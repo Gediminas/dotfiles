7b80f53614d5bb1807e82b225913af55569e5300
Gediminas <gediminas.luzys@gmail.com>
2020-01-07 14:02:50 +0200

    emacs: debugging

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -13,3 +13,20 @@
 ;(enable-theme 'doom-one)
 
 (set-face-attribute 'hl-line nil :background "#BBBBBB")
+
+;(load-file "~/.doom.d/plugins/devenv-emulation.elc")
+;
+
+
+(require 'rtags) ;; optional, must have rtags installed
+(require 'cmake-ide)
+(cmake-ide-setup)
+
+(require 'realgud-node-inspect)
+(global-set-key [f5] 'gud-cont)
+(global-set-key [f4] 'gud-next)
+(global-set-key [f3] 'gud-step)
+(global-set-key [f1] 'gud-break)
+
+(require 'indium)
+(add-hook 'js-mode-hook #'indium-interaction-mode)
