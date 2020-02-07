a5f472523b1209e512ef22fd71fd9843d5990857
Gediminas <gediminas.luzys@gmail.com>
2020-02-07 10:19:22 +0200

    emacs: Debian

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,5 +1,5 @@
 (
- ("default" . ((user-emacs-directory . "~/.emacs.d")))
+ ("default" . ((user-emacs-directory . "~/sub/doom-emacs")))
  ("doom" .    ((user-emacs-directory . "~/sub/doom-emacs")))
  ("space" .   ((user-emacs-directory . "~/sub/spacemacs")))
  ("bare" .    ((user-emacs-directory . "~/.emacs.d.bare")))
