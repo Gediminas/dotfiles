ab929d0168b8d8580342d6347a661b202cd8ee4e
Gediminas <gediminas.luzys@gmail.com>
2020-02-27 05:51:45 +0200

    emacs: Default profile moved to submodule

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,7 +1,8 @@
 (
  ("default" . ((user-emacs-directory . "~/.emacs.d")))
  ("doom" .    ((user-emacs-directory . "~/sub/doom-emacs")))
+ ("2018" .    ((user-emacs-directory . "~/sub/gds-emacs-2018")))
+ ("2020" .    ((user-emacs-directory . "~/sub/gds-emacs-2020")))
+ ("prelude" . ((user-emacs-directory . "~/sub/prelude-emacs")))
  ("space" .   ((user-emacs-directory . "~/sub/spacemacs")))
- ("bare" .    ((user-emacs-directory . "~/.emacs.d.bare")))
- ("old" .     ((user-emacs-directory . "~/.emacs.d.old")))
  )
