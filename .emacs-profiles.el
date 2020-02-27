1a32250a7847a151c025b654f93e783600f35ae4
Gediminas <gediminas.luzys@gmail.com>
2020-02-27 05:34:11 +0200

    emacs: profiles updated

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,5 +1,5 @@
 (
- ("default" . ((user-emacs-directory . "~/sub/doom-emacs")))
+ ("default" . ((user-emacs-directory . "~/.emacs.d")))
  ("doom" .    ((user-emacs-directory . "~/sub/doom-emacs")))
  ("space" .   ((user-emacs-directory . "~/sub/spacemacs")))
  ("bare" .    ((user-emacs-directory . "~/.emacs.d.bare")))
